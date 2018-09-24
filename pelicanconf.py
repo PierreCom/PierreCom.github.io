1f958cc3322a57dc19ca8b180841292b504edb5f
Pierre Comalada <pierre.comalada@gmail.com>
2018-09-25 00:33:06 +0200

    update2

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Pierre Comalada'
-SITEURL = http://PierreCom.github.io
+SITEURL = ''
 SITENAME = 'Pierre Comalada - Portfolio'
 SITELOGO= '/images/profile_logo.png'
 SITETITLE= 'Pierre Comalada'
